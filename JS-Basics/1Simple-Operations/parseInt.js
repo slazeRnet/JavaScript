d073b23ecd77fcccff29ca3e4fdc4e3db5c5b6f9
P.M <vyfullspam50@gmail.com>
2019-10-27 15:28:51 +0300

    Create parseInt.js

---
+++ JS-Basics/1Simple-Operations/parseInt.js
@@ -0,0 +1,10 @@
+function parse(){
+    let initialNumber = 27;
+    let doubleNumber = initialNumber / 4; // 6.75
+    let parsedToInt = parseInt(initialNumber / 4); // 6
+    console.log(doubleNumber);
+    console.log(parsedToInt);
+
+}
+
+parse();
