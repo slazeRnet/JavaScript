1c26b2666f14dbd08a1d5e41fbb309675f78c079
P.M <vyfullspam50@gmail.com>
2019-10-29 01:36:36 +0300

    Create float-or-int.js

---
+++ Tech-module/float-or-int.js
@@ -0,0 +1,8 @@
+var num1 = 601; // int
+var num2 = 7.2; // float
+
+if(num1 % 1 == 0) console.log("int");
+else console.log("float");
+
+if(num2 % 1 == 0) console.log("int");
+else console.log("float"); 
