699be85f0875e9c061f94fa20742e74f7f6801e0
P.M <vyfullspam50@gmail.com>
2019-10-29 10:38:52 +0300

    Create typeof.js

---
+++ Tech-module/typeof.js
@@ -0,0 +1,9 @@
+let num1 = 90;
+let num2 = 4.50;
+let value1 = true;
+let text1 = "Hello";
+
+console.log(typeof num1); // number
+console.log(typeof num2); // number
+console.log(typeof value1); // bool
+console.log(typeof text1); // string
