5a4b3ee2931a773dbdb93d729e21ed1d780b971e
P.M <vyfullspam50@gmail.com>
2019-10-29 10:42:37 +0300

    Create object.js

---
+++ Tech-module/object.js
@@ -0,0 +1,9 @@
+let person = {
+    firstName : "john",
+    lastName : "Travolta",
+    age : 48,
+    town : "New York"
+};
+
+console.log(typeof(person)); // object
+console.log(person.firstName); // John
