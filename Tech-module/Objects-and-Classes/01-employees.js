f5a78964544983cf08b76fbecf5effa6b446dac0
P.M <vyfullspam50@gmail.com>
2020-01-13 18:23:06 +0200

    Create 01-employees.js
    #Classes
    
    #Class Vehicle
    Create a class with name Vehicle that has the following properties:
    type – a string
    model – a string
    parts – an object that contains:
                  -engine – number (quality of the engine)
    	-power – number
    	-quality – engine * power
    fuel – a number
    drive – a function that receives fuel loss and decreases the fuel of the vehicle by that number
    The constructor should receive the type, the model, the parts as an object and the fuel
    In judge post your class (Note: all names should be as described)
    Example
    Test your Vehicle class
    Input	Output
    let parts = {engine: 6, power: 100}
    let vehicle = new Vehicle('a', 'b', parts, 200)
    vehicle.drive(100)
    console.log(vehicle.fuel)
    console.log(vehicle.parts.quality)	100
    600

---
+++ Tech-module/Objects-and-Classes/01-employees.js
@@ -0,0 +1,21 @@
+
+class Vehicle  {
+    constructor(type, model, parts, fuel){
+        this.type = type;
+        this.model = model;
+        this.parts = parts;
+        this.fuel = fuel;
+        this.parts.quality = this.parts.engine * this.parts.power;   
+
+    }
+     //parts.quality = this.parts.engine * this.parts.power;   // error
+     drive(fuelLoss) {
+        this.fuel -= fuelLoss;
+    }
+}
+
+let parts = {engine: 6, power: 100};
+let vehicle = new Vehicle('a', 'b', parts, 200)
+vehicle.drive(100)
+console.log(vehicle.fuel) // 100
+console.log(vehicle.parts.quality) // 600
