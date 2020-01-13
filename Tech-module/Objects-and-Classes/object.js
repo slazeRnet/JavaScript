273604c2be8ef23d1e2ac185dcd771d88127f1e6
P.M <vyfullspam50@gmail.com>
2020-01-13 09:32:51 +0200

    Update object.js

--- Tech-module/Objects-and-Classes/object.js
+++ Tech-module/Objects-and-Classes/object.js
@@ -3,6 +3,16 @@ let person = {
     score : 5,
     age : 17,
     address : 'Sofia, Tsarigradsko shose 11',
-}
+    sayYourName: function() {
+        console.log('Hello my name is ' + this.name);
+        
+    },
+    shortSyntaxMethod(){
+        console.log('Hello my name is ' + this.name);
+    }
+
+};
 
 console.log(person.name + ' - ' + person.score); // Ivan - 5
+person.sayYourName(); //  no need for console.log
+
