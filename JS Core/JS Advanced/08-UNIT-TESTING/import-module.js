50e795bdae1d4f0bc18840fe176697b2a13ecbf0
P.M <vyfullspam50@gmail.com>
2020-02-10 18:32:20 +0200

    Create import-module.js

---
+++ JS Core/JS Advanced/08-UNIT-TESTING/import-module.js
@@ -0,0 +1,7 @@
+//import function
+import toLowerCase from './toLowerCase.js';
+//import whole objects
+import * as myModules from './myModules.js';
+//to change the name after importing
+import {toLowerCase as myModules } from './myModules.js';
+
