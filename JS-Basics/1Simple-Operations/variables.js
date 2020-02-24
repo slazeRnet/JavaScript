651df0a4259bd94d31711fda0aa7f1903594b3b9
Mr. P <vyfullspam50@gmail.com>
2020-02-24 11:04:38 +0200

    js

--- JS-Basics/1Simple-Operations/variables.js
+++ JS-Basics/1Simple-Operations/variables.js
@@ -6,16 +6,50 @@ let c = {
 };
 d = 2;
 
- delete a;
- delete b;
- delete c.age;
- delete d;
+ delete a; // doesnt work
+ delete b;  // doesnt work
+ delete c.age; //  works
+ //delete d; works
 
  console.log(a); // 1
  console.log(b); // 1
  console.log(c); //{ name: 'Mandy' }
- delete c;
+ delete c; // doesnt work
  console.log(c); //{ name: 'Mandy' }
- console.log(d); //error
+ delete c.name; // name prop deleted 
+ console.log(c); //{ }
+ console.log(c.name); // undefined
+ console.log(d); // 2
  
+ function func1(){
+     let sampleLet = 'let';
+     var sampleVar = 'var';
+ }
+
+ //console.log(sampleLet); //sampleLet is not defined
+ //console.log(sampleVar); //sampleVar is not defined
+ function varTest() {
+    var x = 1;
+    {
+      var x = 2;  // same variable!
+      console.log(x);  // 2
+    }
+    console.log(x);  // 2
+  }
+
+ function letTest() {
+    let x = 1;
+    {
+      let x = 2;  // different variable
+      console.log(x);  // 2
+    }
+    console.log(x);  // 1
+  }
+ 
+  var x = 'global';
+let y = 'global';
+console.log('x : ' + this.x); // undefined
+console.log('x : ' + x); // 'global'
+console.log('y : ' + this.y); // undefined
+console.log('y : ' + y); // 'global'
  
