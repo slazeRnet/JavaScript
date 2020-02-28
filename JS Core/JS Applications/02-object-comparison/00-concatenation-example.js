1b07be85448fa6b193c37dfd58a58e74b928ed34
Mr. P <vyfullspam50@gmail.com>
2020-02-28 15:56:59 +0200

    js

--- JS Core/JS Applications/02-object-comparison/00-concatenation-example.js
+++ JS Core/JS Applications/02-object-comparison/00-concatenation-example.js
@@ -5,15 +5,11 @@ let obj = [
     {height: 180}
 ];
 
+// OVERRRITEING THE PROPERTIES OF OBJECTS BY CONCATANATION
 const concatanate = (a, o) => ( {...a, ...o});
 
 const c = obj.reduce(concatanate, {}); //{ name: 'Steven', age: 2, height: 180 }
 
 
 console.log(c);
-
-// OVERWRITEING THE PROPERTIES OF OBJECTS BY CONCATANATION
-
-let {...a} = obj;
-console.log(a);
 
