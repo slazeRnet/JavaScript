8e5cf5b78a8b55bba14e602eb95b719a7fd72991
Mr. P <vyfullspam50@gmail.com>
2020-03-03 11:39:08 +0200

    js

--- JS Core/JS Applications/02-object-comparison/04-extensible-object.js
+++ JS Core/JS Applications/02-object-comparison/04-extensible-object.js
@@ -1,35 +1,44 @@
-function solve(){
+function solve(template){
 
     let myObj = {
       __proto__: {},
       extend: function (template) {
-          let props = Object.getPrototypeOf(template);
-           console.log(props);
-           
+
+        let extendProto = {};
+        let extendObject = {};
+        for(var prop in template){
+
+            if(typeof(template[prop]) == 'function'){
+               extendProto[prop] = template[prop];
+                  
+            }
+            else{
+              extendObject[prop] = template[prop];
+            }
+            
+        }
+        this.__proto__ = extendProto;
+        Object.assign(this, extendObject);
         }
     }
+    myObj.extend(template);
+    
+    
+    
+    
+    //   myObj.extend(template);
+    //   console.log(myObj.__proto__); //Object {extensionMethod: }
+    //   console.log(template); //Object {extensionMethod: , extensionProperty: "someString"}
+    // console.log(myObj); //{ extend: [Function: extend], extensionProperty: 'someString' }
+    //   myObj.__proto__.extensionMethod(); //say something
+    return myObj;
+      
 
-
-    let template = {
+}    
+let template = {
         extensionMethod: function() {console.log(`say something`)},
         extensionProperty: 'someString'
       }
+console.log(solve(template).__proto__); //{ extensionMethod: [Function: extensionMethod] }
 
 
-      for(var prop in template){
-
-          if(typeof(template[prop]) == 'function'){
-             myObj.__proto__.prop = template[prop];
-                console.log(template[prop]);
-                
-          }
-          
-      }
-      console.log(myObj.__proto__);
-      console.log(template);
-      console.log(myObj);
-      template.extensionMethod();
-
-}    
-
-solve();
--- JS Core/JS Applications/02-object-comparison/lab/00-concatenation-example.js
+++ JS Core/JS Applications/02-object-comparison/lab/00-concatenation-example.js
@@ -13,3 +13,24 @@ const c = obj.reduce(concatanate, {}); //{ name: 'Steven', age: 2, height: 180 }
 
 console.log(c);
 
+// USING Object.assign
+
+let obj1 = {
+    name: "Maya",
+    age: 15,
+    assign: function(o){
+        Object.assign(this, o);
+    }
+}
+
+let objToAssign = {
+    email: 'user@web.com'
+}
+obj1.assign(objToAssign);
+console.log(obj1);
+// {
+//     name: 'Maya',
+//     age: 15,
+//     assign: [Function: assign],
+//     email: 'user@web.com'
+//   }
