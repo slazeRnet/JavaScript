11b9a455dce8c993b2822cd80287c08016c834bc
Mr. P <vyfullspam50@gmail.com>
2020-03-26 10:27:04 +0200

    js

--- JS Core/JS Applications/06-remote-databases/BOOKS/app.js
+++ JS Core/JS Applications/06-remote-databases/BOOKS/app.js
@@ -1,1 +1,31 @@
-console.log('My requests...')
+function getAllBooks(){
+    fetch('https://softuni008.firebaseio.com/.json')
+    .then(x => x.json())
+    .then(x =>{
+
+      return x.books;
+
+    }).catch(err => {
+      throw new Error(`${err} : ${err.message}`);
+        
+    });
+    
+}
+
+function loadBooks(){
+    getAllBooks()
+    .then(b => {
+        console.log(b);
+        
+    })
+}
+
+function displayBook(book){
+    let table = document.querySelector('body table');
+
+    console.log(book);
+    
+}
+let loadBooksButton = document.getElementById('loadBooks');
+
+loadBooksButton.addEventListener('click', loadBooks);
