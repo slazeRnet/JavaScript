9bf15c315c2db0034dff4ad708fc5dc2b7b5533c
Mr. P <vyfullspam50@gmail.com>
2020-03-26 16:08:16 +0200

    js

--- JS Core/JS Applications/04-rest-services-and-ajax/01.BUS-STOP/app.js
+++ JS Core/JS Applications/04-rest-services-and-ajax/01.BUS-STOP/app.js
@@ -1,15 +1,45 @@
-import fetch from "node-fetch";
 
 function getInfo() {
     let button = document.getElementById('submit');
-    let stopInputValue = document.querySelector('#stopId');
- 
-    fetch('http://localhost:3000/busInfo')
+    let buses = document.querySelector('body  div div ul');
+    let stopName = document.getElementById('stopName');
+    
     button.addEventListener('click', () => {
+        //removes all buses before each query
+        clearBusList();
+        let stopInputValue = document.querySelector('#stopId').value;
+        let url = `https://busstop-f2d62.firebaseio.com/businfo/${stopInputValue}.json`;
+        fetch(url)
+        .then(x => x.json())
+        .then(x => {
 
-        console.log(busStops);
+            clearBusList();
+            stopName.innerText = x.name;
+            addBusToList(x.buses);
+
+        })
         
     });
 
+    //displays the buses on the buses element
+    function addBusToList(busesObject){
 
+        let busToArray = Object.entries(busesObject);
+
+        //iterates through the buses of the current bus stop and appends them to the buses element
+        for (const bus of busToArray) {
+            
+        let li = document.createElement('li');
+        let content = `Bus ${bus[0]} arrives in ${bus[1]}`;
+        li.innerText = content;
+        
+        buses.appendChild(li);
+        }
+        
+    }
+
+    function clearBusList(){
+        buses.innerHTML = '';
+        stopName.innerText = '';
+    }
 }
