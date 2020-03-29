b69b82266a6b7b5ee79372980af8286ff401b0b4
Mr. P <vyfullspam50@gmail.com>
2020-03-30 00:23:32 +0300

    js

--- JS Core/JS Applications/06-remote-databases/BOOKS/app.js
+++ JS Core/JS Applications/06-remote-databases/BOOKS/app.js
@@ -1,8 +1,61 @@
-import {apiKey} from './apiKey.js';
+import { apiKey } from './apiKey.js';
+import { createBook, listAllBooks } from './requests.js';
+import {appendBook, refreshTable} from './dom-operations.js'
 
-function main(){
-    console.log(apiKey);
+//Load Books
+export async function loadBooks() {
     
+    let books = await listAllBooks()
+        .then(b => {
+           return b.json();
+        })
+        .then(b => {
+           return b;
+
+        });
+
+        console.log(Object.values(books));
+        
+    if (books) {
+        refreshTable();
+        for (const book of Object.entries(books)) { //{title, author, isbn}
+            let id = book[0];
+            let title = book[1].title;
+            let author = book[1].author;
+            let isbn = book[1].isbn;
+            appendBook(title, author, isbn, id)            
+        }
+    }
+}
+
+async function main() {
+    // FORM for creating books
+    let createBookForm = document.querySelector('form');
+    let loadBooksButton = document.querySelector('#loadBooks');
+
+    loadBooksButton.addEventListener('click', (e) => {
+        e.preventDefault();
+        loadBooks();
+    })
+    // Submit 
+    let formButton = createBookForm.elements[3];
+    formButton.addEventListener('click', async (e) => {
+        
+        e.preventDefault();
+        let title = e.target.parentElement.elements['title'].value;
+        let author = e.target.parentElement.elements['author'].value;
+        let isbn = e.target.parentElement.elements['isbn'].value;
+
+        let book = { title, author, isbn };
+        let response = await createBook(book)
+        .then(r => {
+            return r.json();
+        });
+        //refresh table
+        refreshTable();
+        loadBooks();
+    })
+
 }
 
 main();
