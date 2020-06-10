d6f34d6cae1ce03061b01cb2196f37934efcb1b4
P.M <vyfullspam50@gmail.com>
2020-06-10 04:38:52 +0300

    js

--- PowerCurve/scripts/load-data.js
+++ PowerCurve/scripts/load-data.js
@@ -1,11 +1,4 @@
 // pass the json file with the logs as a parameter
 export default {
-    read: function (json){
-            json.forEach(element => {
-    
-            },
-    appendMonth: function (month){
-        
-    }
-}
+   
 }
--- PowerCurve/server.js
+++ PowerCurve/server.js
@@ -1,19 +1,41 @@
-//import controllers from '../controllers/index.js'
+// View engine and Routing engine
 const express = require('express')
-// the DataBase is stored in JSON file
-const fitnessLogs = require('./utils/data.json')
 const pug = require('pug')
 const app = express()
-//script for loading the data 
-let loadData = require('scripts/load-data.js');
+const port = 3001
 var router = express.Router()
-const port = 3000
 const auth = true
+//import { print } from './utils/print-text-body.mjs'
+//import controllers from '../controllers/index.js'
+
+// the DataBase is stored in JSON file
+var fitnessLogs// = require('./utils/data.json')
+const fs = require('fs')
+// read the data synchronosly and store it in the var. If the file is corrupt throw err
+var data = fs.readFile('./utils/data.json', 'utf8', (err, jsonString) => {
+    if (err) {
+          console.error("Error reading DataBase from disk:", err)
+          return
+      }
+      try {
+        console.log(jsonString);
+        
+        fitnessLogs = JSON.parse(jsonString)
+      }
+      catch(err) {
+            console.error('Error parsing JSON string:', err)
+        }
+        })
+        //  print(fitnessLogs)
+        console.log(fitnessLogs);
+        
+        //script for loading the data 
+        //let loadData = require('scripts/load-data.js');
 //set the views and the template engine to Handlebars
 app.set('views', './views')  
 app.set('view engine', 'pug')
 app.use(router)
-app.get('/', async function (req, res) {
+app.get('/', (req, res) => {
      res.render('home/home', fitnessLogs)
 
   })
