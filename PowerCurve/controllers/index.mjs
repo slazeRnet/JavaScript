f9d536e2babb30bb8b5ca00273810a99d9e21a4e
P.M <vyfullspam50@gmail.com>
2020-06-11 02:19:38 +0300

    js

--- PowerCurve/controllers/index.mjs
+++
@@ -1,9 +0,0 @@
-// Import off all the controllers from the folder
-import home from './home.mjs';
-
-// All the controllers are exported
-export default {
-    controllers: {
-        home
-    }
-}
