fe93d1e61fff49b14e426677542fc1d709b3b341
P.M <vyfullspam50@gmail.com>
2020-06-12 04:19:34 +0300

    js

--- PowerCurve/controllers/home.js
+++ PowerCurve/controllers/home.js
@@ -1,12 +1,16 @@
-//import * as express from 'express'
-//const app = express()
+const models = require('../models/index.js')
 
 module.exports = {
 
     get: {
-       home(req, res){
-           res.render('../views/home/home')
-       }
+        //home page controller
+       async home(req, res){
+            //first fetch the json data (Months)
+            let fitnessLogs = await models.workouts.getDataBase()
+            let fitnesLogsJson = await fitnessLogs.json() // the entire fitness logs DB into JSON
+            let months = await models.workouts.getMonthsFromDb(fitnesLogsJson);
+            await res.render('../views/home/home', {months})
+        }
        
     }
 }
--- PowerCurve/models/workouts.js
+++ PowerCurve/models/workouts.js
@@ -1,5 +1,36 @@
-export default {
-    getMonths(timespan){
-        
+const address = require('../utils/json-server-config.js')
+const fetch = require('node-fetch');
+
+module.exports = {
+     getDataBase(){
+         // returns json
+        return fetch(address)
+    },
+    /**
+     @param db {object} is the fitnessLogs db
+     @param timespan {array} first value is the start date, the secound end date
+     */
+    async getMonthsFromDb(db, timespan){
+        let months = {}
+            //iterate through the object and get the year and month in pairs
+            for (var year in db) {
+                if (await Object.prototype.hasOwnProperty.call(db, year)) {
+                    
+                    for (var month in db[year]) {
+                        if (await Object.prototype.hasOwnProperty.call(db[year], month)) {
+                            
+                            // add the months to the array in [year, month] format
+                            if(!months[year]){
+                                months[year] = []
+                            }
+                            if(!months[year].includes(month)){
+                                await months[year].push(month)
+                            }
+                        }
+                    }
+                }
+            }
+            console.log(months)
+            return months
     }
 }
