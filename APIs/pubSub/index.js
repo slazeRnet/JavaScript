b2be61cf1f09b87730b8c135db2442e5d0383dfe
P.M <vyfullspam50@gmail.com>
2021-03-04 01:38:57 +0200

    add img

--- APIs/pubSub/index.js
+++ APIs/pubSub/index.js
@@ -1,16 +1,16 @@
 let express = require('express')
 let app = express();
 const router = express.Router();
-let mustache = require('mustache')
 let path = require('path')
+const port = 3000;
 // let pubSub = require('pubSub')
 
-
-const port = 3000;
+app.use(express.static(path.join(__dirname, 'views')))
+app.set('view engine', 'ejs')
 
 app.get('/', function(req, res){
     // res.sendFile(path.join(__dirname + '/views/index.html'))
-    res.send('All OK!')
+    res.render('index')
 })
 
 app.listen(port) //() => { console.log(`Listening on port ${port}`)
